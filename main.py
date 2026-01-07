a2385c382819a060d9bcc323e5320487609de55a
mkygh_tm2pfj8 <mkygh3192@gmail.com>
2026-01-07 16:47:10 +0200

    changed previous code and finished class AudioCreator

--- main.py
+++ main.py
@@ -1,53 +1,101 @@
-    def generate_piano_wave_with_attack(self, frequency, duration_ms):
-        """Piano sound with hammer attack noise"""
-        duration_s = duration_ms / 1000.0
-        num_samples = int(self.sample_rate * duration_s)
+import pygame
+from random import choice
+
+class AudioCreator:
+    def __init__(self):
+        pygame.mixer.init(frequency=44100,size=-16,channels=1)
+        self.sample_rate=44100
+        self.note_frequencies=[("C",261.63),("C#",277.18),("D",293.66),("D#",311.13),("E",329.63),("F",349.23),("F#",369.99),("G",392.00),("G#",415.30),("A",440.00),("A#",466.16),("B",493.88)]
+
+    def make_octave(self,note:tuple):
+        octave=choice([1,2,3])
+        if octave==1:
+            return note
+        elif octave==2:
+            new_freq=note[1]/2
+            return (note[0],new_freq)
+        else:
+            new_freq=note[1]*2
+            return (note[0],new_freq)
+
+    def make_1note(self):
+        chosen_notes=[]
+        chosen_note=choice(self.note_frequencies)
+        chosen_notes.append(self.make_octave(chosen_note))
+        self.generate_sound_answer(chosen_notes)        
+
+    def make_2notes(self):
+        chosen_notes=[]
+        while True:
+            chosen_note1=choice(self.note_frequencies)
+            chosen_note2=choice(self.note_frequencies)
+            if chosen_note1==chosen_note2:
+                continue
+            else:
+                chosen_notes.append(self.make_octave(chosen_note1))
+                chosen_notes.append(self.make_octave(chosen_note2))
+            self.generate_sound_answer(chosen_notes)         
+            break       
+
+    def make_3notes(self):
+        chosen_notes=[]
+        chosen_note1=choice(self.note_frequencies)
+        note1_index=self.note_frequencies.index(chosen_note1)
+        if choice([1,2])==1:
+            chosen_note2=self.note_frequencies[(note1_index+3)%12]
+            chosen_note3=self.note_frequencies[(note1_index+7)%12]
+        else:
+            chosen_note2=self.note_frequencies[(note1_index+4)%12]
+            chosen_note3=self.note_frequencies[(note1_index+7)%12]
+        chosen_notes.append(self.make_octave(chosen_note1))
+        chosen_notes.append(self.make_octave(chosen_note2))
+        chosen_notes.append(self.make_octave(chosen_note3))
+        self.generate_sound_answer(chosen_notes)         
+
+    def make_4notes(self):
+        chosen_notes=[]
+        chosen_note1=choice(self.note_frequencies)
+        note1_index=self.note_frequencies.index(chosen_note1)
+        if choice([1,2])==1:
+            chosen_note2=self.note_frequencies[(note1_index+4)%12]
+        else:
+            chosen_note2=self.note_frequencies[(note1_index+3)%12]
+        chosen_note3=self.note_frequencies[(note1_index+7)%12]
+        chosen_note4=self.note_frequencies[(note1_index+10)%12]
+        chosen_notes.append(self.make_octave(chosen_note1))
+        chosen_notes.append(self.make_octave(chosen_note2))
+        chosen_notes.append(self.make_octave(chosen_note3))
+        chosen_notes.append(self.make_octave(chosen_note4))
+        self.generate_sound_answer(chosen_notes)         
+
+    def generate_sound_answer(self,chosen_notes:list):
+        for item in chosen_notes:
+            Sound.play(self.make_piano_sound(item[1]))
+        answers=[]
+        for item in chosen_notes:
+            answers.append(item[1])
+        return answers
         
-        # Harmonics with slightly different ratios for realism
-        harmonics = [
-            (1.00, 1.00), (2.00, 0.85), (3.00, 0.70), (4.00, 0.55),
-            (5.00, 0.45), (6.00, 0.35), (7.00, 0.25), (8.00, 0.15),
-            (9.00, 0.10), (10.00, 0.05)
-        ]
+    def make_piano_sound(self,frequency:float,):
+        frequency will be close to piano sound
+
+
         
-        audio_array = array.array('h')
-        
-        for i in range(num_samples):
-            t = i / self.sample_rate
-            
-            # Envelope with faster attack
-            attack = min(t / 0.005, 1.0)  # 5ms attack
-            decay = math.exp(-t * 3)      # Fast decay
-            release = math.exp(-max(0, t - duration_s * 0.7) * 2)  # Release starts at 70%
-            envelope = attack * decay * release
-            
-            # Add slight hammer noise at the very beginning
-            hammer_noise = 0
-            if t < 0.001:  # First 1ms
-                hammer_noise = 0.3 * math.sin(t * 2000) * (1 - t/0.001)
-            
-            # Sum harmonics
-            wave_value = hammer_noise
-            for harmonic_ratio, amplitude in harmonics:
-                # Real strings have slight inharmonicity
-                inharmonicity = 1.0 + 0.0002 * harmonic_ratio * harmonic_ratio
-                harmonic_freq = frequency * harmonic_ratio * inharmonicity
-                
-                # Each harmonic has slightly different envelope
-                harmonic_env = envelope * math.exp(-t * harmonic_ratio * 0.5)
-                wave_value += amplitude * harmonic_env * math.sin(2 * math.pi * harmonic_freq * t)
-            
-            # Normalize
-            wave_value = wave_value / (len(harmonics) + 1)
-            
-            # Soft clipping for warmth
-            if wave_value > 0.9:
-                wave_value = 0.9 + (wave_value - 0.9) * 0.5
-            elif wave_value < -0.9:
-                wave_value = -0.9 + (wave_value + 0.9) * 0.5
-            
-            int_value = int(wave_value * 32767 * 0.8)
-            audio_array.append(int_value)
-            audio_array.append(int_value)
-        
-        return audio_array
+
+
+
+
+
+
+
+
+    def number_of_notes(self):
+        if 1:
+            self.make_1note()
+        elif choice==2:
+            self.make_2notes()
+        elif choice==3:
+            self.make_3notes()
+            ??   (stage1=1note, stage2=2notes, stage3=3, stage4==4 notes)
+
+
